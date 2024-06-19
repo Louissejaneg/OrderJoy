7fbda609fc0a05cd8a73ebef8d6f0240b1e74402
Louissejaneg <21-11705@g.batstate-u.edu.ph>
2024-06-19 16:18:06 +0800

    initiial commit

---
+++ src/main/java/com/Garce/OrderJoy/OrderJoyApplication.java
@@ -0,0 +1,13 @@
+package com.Garce.OrderJoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderJoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderJoyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Garce/OrderJoy/controllers/model/UserModel.java
@@ -0,0 +1,61 @@
+public class UserModel {
+    private int id;
+    private String name;
+    private String email;
+    private Strinig password;
+
+   public UserModel(inr id, String name, String email, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+        //getters
+        public int getId(){
+            return id;
+        }
+
+        public int getName(){
+            return name;
+        }
+        
+        public int getEmail(){
+            return email;
+        }
+
+        public int getPassword(){
+            return password;
+        }
+
+        //setters
+        public void setId(String id){
+            this.Id = Id;
+        }
+        
+        public void setName(String name){
+            this.Name = name;
+        }
+        
+        public void setEmail(String email){
+            this.email = email;
+        }
+        
+        public void setPassword(String password){
+            this.password = password;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
