0cb2e011f396d70e79d8d73ebfc06888a6edec4d
OrderJoy App <21-11705@g..batstate.-u.edu.ph>
2024-06-20 12:09:25 +0800

    Activity.1 User Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title:Center(
-          child:Text('OrderJoy App'),
-        ),
-        backgroundColor: Colors.lightBlue[200],
-      ),
-      body: Center(
-        child: Image.network('https://assets.unileversolutions.com/v1/84326391.jpg?im=AspectCrop=(1440,600);Resize=(1440,600)')
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-          backgroundColor: Colors.blue[200],
-        child: Icon(
-          Icons.add,
-          color: Colors.deepPurple[900],
-        )
-      ),
-    ),
-  ));
-}
-
-
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.cyan[100],
+    appBar: AppBar(
+      title: Text('User Profile'),
+    backgroundColor: Colors.blue[400],
+    centerTitle: true,
+  ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME',
+           style: TextStyle(
+             fontSize: 16.0,
+             letterSpacing: 2.0,
+           ),
+          ),
+          Text(
+            'Louisse Jane Garcia',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 30.0
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR',
+            style: TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            '3rd Year',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 30.0
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'EMAIL',
+            style: TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'Louissegarcia7@gmail.com',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 30.0
+            ),
+          ),
+        ]
+      )
+    )
+)
+));
