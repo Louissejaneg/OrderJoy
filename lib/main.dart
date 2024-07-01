b3a795fa7bdc92646c3bf0c4f4d3acd341346da9
Louissejaneg <21-11705@g.batstate-u.edu.ph>
2024-07-01 11:41:56 +0800

    Sign Up and Validation

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:untitled2/Home.dart';
 import 'package:untitled2/pages/dashboard.dart';
 import 'package:untitled2/pages/menu.dart';
+import 'package:untitled2/pages/signup.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/menu',
+  initialRoute: '/signup',
   routes: {
     '/' : (context) => Dashboard(),
     '/menu' : (context) => Menu(),
     '/profile' : (context) => Home(),
+    '/signup' : (context) => Signup(),
   },
 ));
