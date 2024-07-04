810ac819ec35120dccc30e7df97e2f0d03ab711f
Louissejaneg <21-11705@g.batstate-u.edu.ph>
2024-07-04 11:45:47 +0800

    Activity.4

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled2/services/product.dart';
 import 'package:untitled2/services/menuCard.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -18,6 +19,7 @@ class _MenuState extends State<Menu> {
     final response = await http.get(Uri.parse('http://10.0.2.2:8080/products')
     );
     final data = jsonDecode(response.body);
+    print(data);
     List products = <Product>[];
     for(var product in data){
       products.add(Product.fromJson(product));
@@ -44,6 +46,50 @@ class _MenuState extends State<Menu> {
         backgroundColor: Colors.blue[400],
         centerTitle: true,
       ),
-    );
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: FutureBuilder(
+            future:  products,
+            builder: (context, snapshots){
+              if(snapshots.connectionState == ConnectionState.waiting){
+                return Center(
+                  child: SpinKitFadingCircle(
+                    color: Colors.blue[400],
+                    size: 80.0,
+                  ),
+                );
+              }
+              if(snapshots.hasData) {
+                List products = snapshots.data!;
+                return Padding(
+                  padding: EdgeInsets.all(3.0),
+                  child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index){
+                      return Card(
+                        child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                            Text(products[index].productName,
+                            style: TextStyle(fontWeight : FontWeight.bold)
+                            ),
+                            Text(products[index].price.toString())
+                            ],
+                          ),
+                          onTap: (){},
+                        ),
+                      );
+                    }
+                  ),
+                );
+              }
+              return Center(
+                child: Text('Unable to load data'),
+              );
+            }
+        ),
+        ),
+      );
   }
 }
