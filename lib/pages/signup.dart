22df2b3ebdce86a0fbdc03f55a4729dd964b4f18
Louissejaneg <21-11705@g.batstate-u.edu.ph>
2024-07-09 15:52:57 +0800

    May Mata

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -16,6 +16,8 @@ class _SignupState extends State<Signup> {
   String name = '';
   String email = '';
   String password = '';
+  bool _obscure = true;
+  IconData _obscureIcon = Icons.visibility_off;
 
 createAccount(User user) async{
   final response = await http.post(
@@ -102,8 +104,22 @@ createAccount(User user) async{
                     ),
                     SizedBox(height: 20.0,),
                     TextFormField(
-                      obscureText: true,
+                      obscureText: _obscure,
                       decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock_rounded),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureIcon),
+                          onPressed: (){
+                            setState(() {
+                              _obscure = !_obscure;
+                              if(_obscure){
+                                _obscureIcon = Icons.visibility_off;
+                              }else{
+                                _obscureIcon = Icons.visibility;
+                              }
+                            });
+                          },
+                        ),
                         label: Text('Password'),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20.0)
